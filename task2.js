61ea0bf9d64aa3777a76f219667df997df86d22e
Loreta <macerdava@gmail.com>
2025-11-07 19:07:50 -0300

    feat: task 3

--- task2.js
+++ task2.js
@@ -3,7 +3,7 @@
 for (let i = 1; i <= 105; i++) {
   if (i % 3 === 0) {
     
-    console.log("Fizz");
+    console.log("Fizz⚡");
     
   } else {
     console.log(i);
--- task3.js
+++ task3.js
@@ -1,5 +1,18 @@
-// Refer to Task 3 in your Instructions to complete this task
+// Refer to Task 1 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Three!");
-  };
+for (let i = 1; i <= 105; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    
+    console.log("FizzBuzz⚡💥");
+    
+  } else if(i % 3 === 0){
+    console.log("Fizz⚡");
+  } 
+  
+  else if(i % 5 === 0){
+    console.log("Buzz💥");
+  } 
+  else {
+    console.log(i);
+  }
+}
